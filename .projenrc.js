b02c2668f03e30caab6e83fd331fa6073e92eaa7
Austin <austin@effective-computing.com>
2020-10-18 13:24:49 +0200

    npx projen after dependencies and git ignore file added

--- .projenrc.js
+++ .projenrc.js
@@ -2,7 +2,20 @@ const { AwsCdkTypeScriptApp } = require('projen');
 
 const project = new AwsCdkTypeScriptApp({
   cdkVersion: "1.63.0",
-  name: "Projen"
+  name: "Projen",
+  authorName: "Austin Dimmer",
+  authorOrganization: "Effective Computing Ltd",
+  cdkDependencies: [
+    "@aws-cdk/aws-codebuild",
+    "@aws-cdk/aws-codepipeline-actions",
+  ],
+  devDependencies: {
+    // "aws-sdk": Semver.caret("2.708.0")
+  },
+  gitignore: [
+    "BootstrapCdkPipelines.bat",
+    "bootstrap.log.txt",
+  ]
 });
 
 project.synth();
